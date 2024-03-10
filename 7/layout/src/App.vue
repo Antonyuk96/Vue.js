8cd472ed085daa0e6d00a1f7a9185a5fd6879e8b
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2024-03-10 23:56:31 +0300

    Add files via upload

---
+++ 7/layout/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <ProjectPage />
+
+    <HomePage />
+
+    <BlogPage />
+
+    <BlogDetailsPage />
+  </div>
+</template>
+
+<script>
+
+import homepage from './pages/homepage.vue'
+
+import blogPage from './pages/blogpage.vue'
+
+import blogdetailspage from './pages/blogdetailspage.vue'
+
+import projectpage from './pages/projectpage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    homepage,
+
+    blogpage,
+
+    blogdetailspage,
+
+    projectpage
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "scss/vars";
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.center {
+  padding-left: calc(50% - $widthSite / 2);
+  padding-right: calc(50% - $widthSite / 2);
+}
+</style>
---
+++ 7/layout/src/pages/homepage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <HeaderComponent></HeaderComponent>
+    <HomePageBaner></HomePageBaner>
+    <HomePageProject></HomePageProject>
+    <HomePageCounter></HomePageCounter>
+    <HomePageArticles></HomePageArticles>
+    <FooterComponent></FooterComponent>
+  </div>
+</template>
+
+<script>
+import headercomponent from '../components/headercomponent.vue'
+import homepagebanner from '../components/pagescomponents/homepage/homepagebanner.vue'
+import homepageproject from '../components/pagescomponents/homepage/homepageproject.vue'
+import homepagecounter from '../components/pagescomponents/homepage/homepagecounter.vue'
+import homepagearticles from '../components/pagescomponents/homepage/homepagearticles.vue'
+import footercomponent from '../components/footercomponent.vue'
+
+export default {
+  name: 'homepage',
+
+  components: {
+    headercomponent,
+    homepagebanner,
+    homepageproject,
+    homepagecounter,
+    homepagearticles,
+    footercomponent
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
